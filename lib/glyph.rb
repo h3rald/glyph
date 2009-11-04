f8906fe87015510fb66b5d07f3c7e188543c1ab7
h3rald <h3rald@h3rald.com>
2009-11-04 17:06:28 +0100

    Main module.

---
+++ lib/glyph.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# Glyph
+#
+# @website http://www.h3rald.com/glyph
+# @author Fabio Cevasco (mailto:h3rald@h3rald.com)
+# @copyright Copyright (c) 2009 Fabio Cevasco
+# @license BSD
+
+require 'rubygems'
+require 'pathname'
+require 'yaml'
+require 'gli'
+require 'extlib'
+
+lib = Pathname(__FILE__).dirname.expand_path
+core = lib/'glyph'
+
+module Glyph
+
+end
