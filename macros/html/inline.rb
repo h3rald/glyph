ee0d8dd154e35579fab408e18f483e559976351c
h3rald <h3rald@h3rald.com>
2010-02-01 17:11:10 +0100

    Implemented table[], tr[], td[], th[].

--- macros/html/inline.rb
+++ macros/html/inline.rb
@@ -25,8 +25,46 @@ macro :link do |node|
 	%{<a href="#{params[0]}">#{params[1]}</a>}
 end
 
+macro :term do |node|
+	# TODO
+end
+
+macro :fig do |node|
+	# TODO
+end
+
+macro :biblio do |node|
+	# TODO
+end
+
+macro :fn do |node|
+	# TODO
+end
+
+macro :code do |node|
+	# TODO
+	node[:value]
+end
+
+macro :table do |node|
+	%{<table>#{node[:value]}</table>}
+end
+
+macro :tr do |node|
+	%{<tr>#{node[:value]}</tr>}
+end
+
+macro :td do |node|
+	%{<td>#{node[:value]}</td>}
+end
+
+macro :th do |node|
+	%{<th>#{node[:value]}</th>}
+end
+
 macro_alias :important => :note
 macro_alias :tip => :note
 macro_alias :caution => :note
 macro_alias '#' => :anchor
 macro_alias '=>' => :link
+macro_alias '+' => :term
