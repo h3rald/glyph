5414f2240f65613e9e1bcfdc8550aa8687b6230d
h3rald <h3rald@h3rald.com>
2010-08-03 15:24:54 +0200

    Started implementing 'glyph stats' command.

--- spec/tasks/project_spec.rb
+++ spec/tasks/project_spec.rb
@@ -11,11 +11,11 @@ describe "project:create" do
 		delete_project_dir
 	end
 
-	it "[create] should not create a new project if no valid directory is supplied" do
+	it "should not create a new project if no valid directory is supplied" do
 		lambda { Glyph.run! 'project:create', 'test_dir' }.should raise_error
 	end
 
-	it "[create] should create a new project if an existing empty directory is supplied" do
+	it "should create a new project if an existing empty directory is supplied" do
 		lambda { Glyph.run! 'project:create', Glyph::PROJECT }.should_not raise_error
 		(Glyph::PROJECT/'lib').exist?.should == true
 		(Glyph::PROJECT/'document.glyph').exist?.should == true
@@ -26,13 +26,25 @@ describe "project:create" do
 		(Glyph::PROJECT/'output').exist?.should == true
 	end
 
-	it "[create] should create a project in a directory containing just Gemfiles or hidden files" do
+	it "should create a project in a directory containing just Gemfiles or hidden files" do
 		file_write Glyph::PROJECT/".test", "..." 
 		file_write Glyph::PROJECT/"Gemfile", "..." 
 		lambda { Glyph.run! 'project:create', Glyph::PROJECT }.should_not raise_error
 	end
+end
 
-	it "[add] should add new files to project" do
+describe "project:add" do
+
+	before do
+		create_project_dir
+	end
+
+	after do
+		delete_project_dir
+	end
+
+
+	it "should add new files to project" do
 		create_project
 		lambda { Glyph.run 'project:add', 'test.textile'}.should_not raise_error
 		(Glyph::PROJECT/'text/test.textile').exist?.should == true
@@ -40,5 +52,50 @@ describe "project:create" do
 		lambda { Glyph.run 'project:add', 'chapter1/test.textile'}.should_not raise_error
 		(Glyph::PROJECT/'text/chapter1/test.textile').exist?.should == true
 	end
+
+end
+
+describe "project:stats" do
+	
+	before do
+		reset_quiet
+		create_project_dir
+		create_project
+		Glyph.file_copy Glyph::SPEC_DIR/'files/document_for_stats.glyph', Glyph::PROJECT/'document.glyph'
+		Glyph.file_copy Glyph::SPEC_DIR/'files/references.glyph', Glyph::PROJECT/'text/references.glyph'
+	end
+
+	after do
+		delete_project_dir
+	end
+
+	it "should populate Glyph::STATS and generate a Glyph document" do
+		lambda { Glyph.run 'project:stats' }.should_not raise_error
+		Glyph.document.blank?.should == false
+		Glyph::STATS.blank?.should == false
+	end
+
+	it "should collect macro stats" do
+		Glyph.run "project:stats", :macros
+		Glyph::STATS[:macros].blank?.should == false
+		Glyph::STATS[:macros].should == {
+			:total_definitions => 9,
+			:total_instances => 19,
+			:definitions => [:"snippet:", :toc, :include, :textile, 
+				:section, :markdown, :snippet, :"#", :"=>"]
+		}
+		# TODO: stats on a specific macro.
+	end
+
+	it "should collect bookmark stats"
+
+	it "should collect link stats"
+	
+	it "should collect snippet stats"
+
+	it "should collect file stats"
+
+	it "should collect global stats"
+	
 
 end
--- tasks/project.rake
+++ tasks/project.rake
@@ -33,4 +33,51 @@ namespace :project do
 		File.new(file.to_s, "w").close
 	end
 
+	desc "Display project statistics"
+	task :stats, :object, :value, :parameter, :needs => ["generate:document"]  do |t, args|
+		stats = Glyph::STATS
+		stats[:macros] = {}
+		stats[:bookmarks] = {}
+		stats[:links] = {}
+		stats[:snippets] = {}
+		stats[:files] = {}
+		stats[:global] = {}
+		get_macros = lambda do |name|
+			nodes = []
+			Glyph.document.structure.descend do |n, level|
+				if n.is_a? Glyph::MacroNode then
+					nodes << n if !name || Glyph.macro_eq?(name, n[:name])
+				end
+			end
+			nodes
+		end
+		c_stats = args[:object] ? stats[args[:object]] : stats[:all]
+		case args[:object]
+		when :macros
+			macros = get_macros.call nil
+			c_stats[:total_instances] = macros.length			
+			definitions = macros.map{|n| n[:name] }.uniq
+			uniqs = definitions.dup
+			definitions.delete_if do |el| 
+				found = false
+				uniqs.each do |name| 
+					if Glyph.macro_eq? name, el then
+						found = true
+						break
+					end
+				end
+			end
+			c_stats[:definitions] = definitions
+			c_stats[:total_definitions] = definitions.length
+		when :macro
+		when :bookmarks
+		when :bookmark
+		when :links
+		when :topics
+		when :snippets
+		when :snippet
+		else
+		end
+	end
+
 end
